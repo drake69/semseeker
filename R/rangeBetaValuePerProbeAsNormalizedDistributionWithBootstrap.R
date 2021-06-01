08cc46701f1711405a9cf7466dfb78e2546f956d
lcorsaro <lcorsaro@aulamed1.unipv.it>
2021-06-01 16:21:27 +0200

    test of bootstrap

--- R/rangeBetaValuePerProbeAsNormalizedDistributionWithBootstrap.R
+++ R/rangeBetaValuePerProbeAsNormalizedDistributionWithBootstrap.R
@@ -6,7 +6,8 @@
 #'
 #' @return methylation matrix as normalized distribution
 
-rangeBetaValuePerProbeAsNormalizedDistribution <- function(populationMatrix, iqrTimes = 3) {
+rangeBetaValuePerProbeAsNormalizedDistributionWithBootstrap <- function(populationMatrix, iqrTimes = 3) {
+
   computationCluster <- parallel::makeCluster(parallel::detectCores(all.tests = FALSE, logical = TRUE) - 1)
   doParallel::registerDoParallel(computationCluster)
 
