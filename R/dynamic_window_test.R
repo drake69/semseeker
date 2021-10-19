7fc31708ae99b2a8139588a88d2c3b807c5cb74a
Luigi Corsaro <l.corsaro@a-company.biz>
2021-10-19 08:48:09 +0200

    renamed df to tempDataFrame

--- R/dynamic_window_test.R
+++ R/dynamic_window_test.R
@@ -4,11 +4,11 @@
 # n <- dim(PROBES)[1]
 # epimutation <- sample(c(0,1), replace=TRUE, size=n)
 #
-# df <- data.frame(PROBES, MUTATIONS = epimutation)
+# tempDataFrame <- data.frame(PROBES, MUTATIONS = epimutation)
 #
-# df <- subset(df, CHR=="11")
+# tempDataFrame <- subset(tempDataFrame, CHR=="11")
 #
-# start <- sort(df$START, decreasing = FALSE)
+# start <- sort(tempDataFrame$START, decreasing = FALSE)
 #
 # left <- slide_index(start, start, ~length(.x), .after=5000, .complete = TRUE)
 # rigth <- slide_index(start, start, ~length(.x), .before = 10000, .complete = TRUE)
@@ -29,7 +29,7 @@
 #
 #
 #
-# df <- data.frame(
+# tempDataFrame <- data.frame(
 #   y = rnorm(100),
 #   x = rnorm(100),
 #   i = as.Date("2019-08-15") + c(0, 2, 4, 6:102) # <- irregular
@@ -37,7 +37,7 @@
 #
 # # 20 day rolling regression. Current day + 19 days back.
 # # Additionally, set `.complete = TRUE` to not compute partial results.
-# regr <- slide_index(df, df$i, ~lm(y ~ x, .x), .before = 19, .complete = TRUE)
+# regr <- slide_index(tempDataFrame, tempDataFrame$i, ~lm(y ~ x, .x), .before = 19, .complete = TRUE)
 #
 #
 # i <- c(2017, 2017, 2018, 2019, 2020, 2020)
