96c8445d14e59365e0bf62a7bc31b6e6aa041df5
Luigi Corsaro <l.corsaro@a-company.biz>
2021-11-19 18:39:25 +0100

    drafted vcreate excel pivot test

--- tests/testthat/test-createExcelPivot.R
+++ tests/testthat/test-createExcelPivot.R
@@ -1,3 +1,27 @@
 test_that("createExcelPivot", {
 
+  populations <- c("Reference","Control","Case")
+  figures <- c("HYPO", "HYPER", "BOTH")
+  anomalies <- c("MUTATIONS","LESIONS")
+
+  subGroups <- c("Body","TSS1500","5UTR","TSS200","1stExon","3UTR","ExonBnd","Whole")
+  probesPrefix = "PROBES_Gene_"
+  mainGroupLabel =  "GENE"
+  subGroupLabel="GROUP"
+
+  createExcelPivot ( populations =  populations, figures =  figures,anomalies =  anomalies, subGroups =  subGroups, probesPrefix =   probesPrefix, mainGroupLabel =  mainGroupLabel, subGroupLabel =  subGroupLabel)
+
+
+  probesPrefix <- "PROBES_Island_"
+  subGroups <- c("N_Shore","S_Shore","N_Shelf","S_Shelf","Island", "Whole")
+  mainGroupLabel <- "ISLAND"
+  subGroupLabel <- "RELATION_TO_CPGISLAND"
+  createExcelPivot ( populations, figures, anomalies, subGroups, probesPrefix, mainGroupLabel, subGroupLabel)
+
+  subGroups <- c("DMR")
+  probesPrefix = "PROBES_DMR_"
+  mainGroupLabel =  "DMR"
+  subGroupLabel="GROUP"
+  createExcelPivot (populations, figures, anomalies, subGroups, probesPrefix, mainGroupLabel, subGroupLabel)
+
 })
