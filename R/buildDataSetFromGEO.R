6949df6e0a402c7e663ae92525619e87a454c1b5
Luigi Corsaro <l.corsaro@a-company.it>
2022-02-06 16:16:50 +0100

    Added utility to download and import data directly from GEO. Gene Expression Omnibus

---
+++ R/buildDataSetFromGEO.R
@@ -0,0 +1,80 @@
+#' buildDataSetFromGEO
+#'
+#' @param GEOgse geo accession dataset identification
+#' @param workingFolder where sample sheet and files will be saved
+#' @param downloadFiles true if you wnatr download files from Gene Expression Ombibus (GEO)
+#'
+#' @return samplesheet, and sample's file saved and samplesheet tab separated saved
+#' @export
+#'
+#' @examples buildDataSetFromGEO("GSE186766","~/", TRUE)
+buildDataSetFromGEO <-  function(GEOgse, workingFolder, downloadFiles = FALSE) {
+
+    gse <- GEOquery::getGEO(GEOgse, GSEMatrix = TRUE)
+
+    phenoDataName <-  paste(GEOgse, "_series_matrix.txt.gz", sep = "")
+    samplesheet <- gse[[phenoDataName]]@phenoData@data
+
+    samplesheet$Sample_ID <- samplesheet$geo_accession
+    samplesheet$Sample_Group <- ""
+    samplesheet$Sentrix_ID <- ""
+    samplesheet$Sentrix_Position <- ""
+
+    for(sample in 1:nrow(samplesheet))
+    {
+      # sample <- 1
+      print(sample)
+      fileName <- samplesheet [sample,"supplementary_file"]
+      tempData <-  gsub("ftp://ftp.ncbi.nlm.nih.gov/geo/samples/", "", fileName)
+      tempData <-  gsub("suppl/", "", tempData)
+      tempData <-  gsub("_Grn.idat.gz", " ", tempData)
+      tempData <-  gsub("_Red.idat.gz", " ", tempData)
+      tempData <-  noquote(strsplit(tempData, "/"))
+      tempData <-  strsplit(tempData[[1]], " ")
+      tempData <-  tempData[length(tempData)] [[1]]
+      tempData <-  unlist(strsplit(tempData, "_"))
+      Sentrix_ID <- tempData[length(tempData)-1]
+      Sentrix_Position <- tempData[length(tempData)]
+      samplesheet [sample,"Sentrix_ID"] <- Sentrix_ID
+      samplesheet [sample,"Sentrix_Position"] <- Sentrix_Position
+
+      if (downloadFiles) {
+
+        fileName <-  gsub("_Grn.idat.gz", "_COLOR.idat.gz", fileName)
+        fileName <-  gsub("_Red.idat.gz", "_COLOR.idat.gz", fileName)
+
+        for (color in c("_Grn.idat.gz","_Red.idat.gz"))
+        {
+          # color <- "_Grn.idat.gz"
+          localFileName <- paste(Sentrix_ID,"_", Sentrix_Position, color, sep="")
+          localFileName <- paste(workingFolder, "/", localFileName, sep = "")
+          localFileNameUnzipped <- gsub(".gz","",localFileName)
+          if(!file.exists(localFileName) &&  !file.exists(localFileNameUnzipped))
+          {
+            fileName <-  gsub("_COLOR.idat.gz",color, fileName)
+            download.file(
+              url = fileName,
+              destfile =  localFileName,
+              # method =  "internal",
+              quiet = FALSE,
+              mode = "wb",
+              cacheOK = TRUE,
+              extra = getOption("download.file.extra"),
+              headers = NULL
+            )
+            GEOquery::gunzip(localFileName, overwrite = TRUE)
+          }
+        }
+      }
+    }
+
+    write.table(
+      samplesheet,
+      paste(workingFolder, "/", "final_samplesheet.csv", sep = ""),
+      row.names = FALSE,
+      sep="\t",
+      quote = FALSE
+    )
+
+    return(samplesheet)
+}
