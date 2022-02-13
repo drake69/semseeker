44316a4a7881b2fdbd5979ed5bd77253da5ba6c7
Luigi Corsaro <l.corsaro@a-company.it>
2022-02-13 20:10:00 +0100

    managed global variables

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,18 @@
-# if(getRversion() >= "2.15.1")  utils::globalVariables(c( "resultFolderData","chartFolder","logFolder","computationCluster",
-#                                                          "resultFolderChart","resultFolderInference","resultFolderEuristic",
-#                                                          "keys.anomalies","keys.figures","keys.populations","keys",
-#                                                          "probes"))
+if(getRversion() >= "2.15.1")  utils::globalVariables(c( "resultFolderData","chartFolder","logFolder","computationCluster",
+                                                         "resultFolderChart","resultFolderInference","resultFolderEuristic",
+                                                         "keys.anomalies","keys.figures","keys.populations","keys",
+                                                         "probes"))
+
+resultFolderData <- NULL
+chartFolder <- NULL
+logFolder <- NULL
+computationCluster <- NULL
+resultFolderChart <- NULL
+resultFolderInference<- NULL
+resultFolderEuristic <- NULL
+keys.anomalies <- NULL
+keys.figures <- NULL
+keys.populations<- NULL
+keys <- NULL
+probes <- NULL
+foreachIndex <- NULL
