f7ac21800d34f40091b14fd7f28c36f4228c0a94
Luigi Corsaro <l.corsaro@a-company.it>
2022-02-17 21:58:23 +0100

    unused function cleanup

--- R/wrap_it.R
+++
@@ -1,9 +0,0 @@
-# Core wrapping function
-wrap_it <- function(x, len)
-{
-  sapply(x, function(y) paste0(strwrap(y, len),
-                              collapse = "\n"),
-         USE.NAMES = FALSE)
-}
-
-
