1d5d3b51f60b7c173b25f93176b155b4681ba716
Luigi Corsaro <l.corsaro@a-company.it>
2022-02-17 16:23:04 +0100

    cleaned up unused funnctions

--- R/flattenCorrMatrix.R
+++
@@ -1,9 +0,0 @@
-flattenCorrMatrix <- function(cormat, pmat) {
-  ut <- upper.tri(cormat)
-  data.frame(
-    row = rownames(cormat)[row(cormat)[ut]],
-    column = rownames(cormat)[col(cormat)[ut]],
-    cor  =(cormat)[ut],
-    p = pmat[ut]
-  )
-}
