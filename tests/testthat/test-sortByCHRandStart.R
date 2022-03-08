9bd7641e6d636e5759ed8e64d477c08e781f7df6
Luigi Corsaro <l.corsaro@a-company.it>
2022-03-08 13:00:02 +0100

    created test sortByCHRandStart

---
+++ tests/testthat/test-sortByCHRandStart.R
@@ -0,0 +1,17 @@
+test_that("sortByCHRandSTART", {
+
+  nitem <- 5e2
+
+  probeFeatures <- PROBES[!is.na(PROBES$CHR),]
+  probeFeatures <- probeFeatures[probeFeatures$PROBE %in% sample(x=probeFeatures[,"PROBE"] , size=nitem),]
+
+  probeFeatures$ABSOLUTE <- paste(probeFeatures$CHR, probeFeatures$START, sep="_")
+
+  #order not matching
+  second <- sortByCHRandSTART( probeFeatures[order(probeFeatures$START),])
+
+  expect_true( test_match_order( sortByCHRandSTART(probeFeatures)$ABSOLUTE,second$ABSOLUTE)
+                                 )
+
+}
+)
