89e81d292f3aaca26b7104506cda278e1e470ef0
Luigi Corsaro <l.corsaro@a-company.it>
2022-03-10 14:04:36 +0100

    corrected error on subset of data, added test to create excel pivot

--- tests/testthat/test-createExcelPivot.R
+++ tests/testthat/test-createExcelPivot.R
@@ -1,17 +1,72 @@
-# test_that("createExcelPivot", {
-#
-#   init_env()
-#   populations <- c("Reference","Control","Case")
-#   figures <- c("HYPO", "HYPER", "BOTH")
-#   anomalies <- c("MUTATIONS","LESIONS")
-#
-#   subGroups <- c("Body","TSS1500","5UTR","TSS200","1stExon","3UTR","ExonBnd","Whole")
-#   probesPrefix = "PROBES_Gene_"
-#   mainGroupLabel =  "GENE"
-#   subGroupLabel="GROUP"
-#   createExcelPivot ( populations =  populations, figures =  figures,anomalies =  anomalies, subGroups =  subGroups, probesPrefix =   probesPrefix, mainGroupLabel =  mainGroupLabel, subGroupLabel =  subGroupLabel)
-#
-#
+test_that("createExcelPivot", {
+
+  library(stringi)
+  tempFolder <- paste("/tmp/semseeker/",stringi::stri_rand_strings(1, 7, pattern = "[A-Za-z0-9]"),sep="")
+  envir <- init_env(tempFolder)
+
+  nitem <- 5e4
+  nsamples <- 5
+  methylationData <- rnorm(nitem*nsamples,mean = 0.5, sd = 0.7)
+  methylationData <- as.data.frame(matrix(methylationData,nitem,nsamples))
+  probeFeatures <- PROBES[!is.na(PROBES$CHR),]
+  probeFeatures <- probeFeatures[probeFeatures$PROBE %in% sample(x=probeFeatures[,"PROBE"] , size=nitem),]
+
+  betaSuperiorThresholds <- data.frame(rnorm(nitem, mean = 1, sd=0.2))
+  betaInferiorThresholds <- data.frame(rnorm(nitem, mean=0.2, sd=0.2))
+
+  row.names(betaSuperiorThresholds) <- probeFeatures$PROBE
+  row.names(betaInferiorThresholds) <- probeFeatures$PROBE
+  row.names(methylationData) <- probeFeatures$PROBE
+
+  Sample_ID <- stringi::stri_rand_strings(nsamples, 7, pattern = "[A-Za-z]")
+  colnames(methylationData) <- Sample_ID
+  Sample_Group <- rep("Control",nsamples)
+  sampleSheet <- data.frame(Sample_Group, Sample_ID)
+
+  sp <- analizePopulation(methylationData=methylationData,
+                          slidingWindowSize = 11,
+                          betaSuperiorThresholds = betaSuperiorThresholds,
+                          betaInferiorThresholds = betaInferiorThresholds,
+                          sampleSheet = sampleSheet,
+                          betaMedians = betaSuperiorThresholds - betaInferiorThresholds,
+                          bonferroniThreshold = 0.01,
+                          probeFeatures = probeFeatures
+  )
+
+  createMultipleBed(envir, sampleSheet = sampleSheet)
+
+  populations <- c("Control")
+
+  figures <- c("HYPO", "HYPER", "BOTH")
+  anomalies <- c("MUTATIONS","LESIONS")
+
+  groups <- c("Body","TSS1500","5UTR","TSS200","1stExon","3UTR","ExonBnd","Whole")
+  probesPrefix = "PROBES_Gene_"
+  columnLabel =  "GENE"
+  groupingColumnLabel="GROUP"
+
+  # create and read
+  finalBed <- annotateBed (
+    populations ,
+    figures ,
+    anomalies ,
+    groups ,
+    probesPrefix ,
+    columnLabel ,
+    groupingColumnLabel)
+
+  populations <- c("Reference","Control","Case")
+  figures <- c("HYPO", "HYPER", "BOTH")
+  anomalies <- c("MUTATIONS","LESIONS")
+
+  subGroups <- c("Body","TSS1500","5UTR","TSS200","1stExon","3UTR","ExonBnd","Whole")
+  probesPrefix = "PROBES_Gene_"
+  mainGroupLabel =  "GENE"
+  subGroupLabel="GROUP"
+  createExcelPivot ( populations =  populations, figures =  figures,anomalies =  anomalies, subGroups =  subGroups, probesPrefix =   probesPrefix, mainGroupLabel =  mainGroupLabel, subGroupLabel =  subGroupLabel)
+
+  expect_true(file.exists(file.path(ssEnv$resultFolderData,"Pivots/GENE.xlsx")))
+  #
 #
 #   probesPrefix <- "PROBES_Island_"
 #   subGroups <- c("N_Shore","S_Shore","N_Shelf","S_Shelf","Island", "Whole")
@@ -24,6 +79,6 @@
 #   mainGroupLabel =  "DMR"
 #   subGroupLabel="GROUP"
 #   createExcelPivot (populations, figures, anomalies, subGroups, probesPrefix, mainGroupLabel, subGroupLabel)
-#
-#
-# })
+
+
+})
