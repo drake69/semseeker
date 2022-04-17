a445c8cece89023431ebdb48a51b12a53fc7f3b8
Luigi Corsaro <l.corsaro@a-company.it>
2022-04-17 10:51:44 +0200

    improved control over dstaframe

--- R/createHeatmap.R
+++ R/createHeatmap.R
@@ -45,91 +45,92 @@ create_heatmap <-
     variables_to_export <- c("anomalies", "inputBedDataFrame", "pops", "groupLabel", "chartFolder")
     g <- 0
     foreach::foreach(g = 1:length(anomalies), .export = variables_to_export) %dorng%
-    # for(g in 1:length(anomalies))
-    # for (anomaly in anomalies)
-    {
-
-      anomaly <- anomalies[g]
-      # tempDataFrame <- subset(inputBedDataFrame, ANOMALY == anomaly)
-      # if(dim(tempDataFrame)[1]==0)
-      #   next
-      # tempDataFrame <- reshape2::dcast(data = tempDataFrame, SAMPLEID ~ KEY, value.var = "FREQ", sum)
-      # mine.long <- tidyr::pivot_longer(data = tempDataFrame,
-      #                           cols = -c(1),
-      #                           names_to = "KEY",
-      #                           values_to = "FREQ")
-      # # head(mine.long)
-      # fillLable <- paste0("Number of ", anomaly, sep="" )
-      # mine.heatmap <- ggplot2::ggplot(data = mine.long,mapping = ggplot2::aes(x = KEY,y = SAMPLEID, fill = FREQ))
-      # mine.heatmap <- mine.heatmap +  ggplot2::geom_tile()
-      # mine.heatmap <- mine.heatmap + ggplot2::xlab(label = groupLabel)
-      # mine.heatmap <- mine.heatmap + ggplot2::ylab(label = "SAMPLE NAME")
-      # mine.heatmap <- mine.heatmap +  ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
-      #
-      # # filename = paste0( chartFolder,"/",paste0( pops, collapse ="_Vs_"),"_", groupLabel,"_",anomaly, ".png",sep="")
-      # # grDevices::png(file= filename, width=1200, height=1200)
-      # # mine.heatmap
-      # # grDevices::dev.off()
-      #
-      # filename = paste0( chartFolder,"/",paste0( pops, collapse ="_Vs_"),"_", groupLabel,"_",anomaly, ".png",sep="")
-      #
-      # try(
-      #   ggplot2::ggsave(
-      #     filename = filename,
-      #     plot = mine.heatmap,
-      #     device = NULL,
-      #     path = NULL,
-      #     scale = 1,
-      #     width = NA,
-      #     height = NA,
-      #     units = c("in", "cm", "mm"),
-      #     dpi = 1200,
-      #     limitsize = TRUE
-      #   )
-      # )
-
-      tempDataFrame <- subset(inputBedDataFrame, inputBedDataFrame$ANOMALY == anomaly)
-      if(nrow(tempDataFrame)>0)
+      # for(g in 1:length(anomalies))
+      # for (anomaly in anomalies)
       {
-        tempDataFrame <- reshape2::dcast(data = tempDataFrame, SAMPLEID + POPULATION ~ KEY, value.var = "FREQ", sum)
-        row.names(tempDataFrame) <- tempDataFrame$SAMPLEID
 
-        mainTitle <- paste0( paste0( pops, collapse ="_Vs_")," ", groupLabel," ",anomaly, sep="")
-        if(nrow(tempDataFrame)>1000 || ncol(tempDataFrame)>1000)
-        {
-          #reduce
-          temp2 <- as.matrix(tempDataFrame[,3:dim(tempDataFrame)[2]])
-          temp <- apply(temp2,2, sum)
-          temp1 <- sort(temp, decreasing = T)
-          limit <- temp1[1000]
-          if(sum(temp1==limit)>1)
-            limit <- limit + 1
-          tempDataFrame <- data.frame(tempDataFrame[,1:2], temp2[,temp1 > limit])
-          rm(temp)
-          rm(temp1)
-          rm(temp2)
-          mainTitle <- paste0( mainTitle," (first 1000)",  sep="")
-        }
+        anomaly <- anomalies[g]
+        # tempDataFrame <- subset(inputBedDataFrame, ANOMALY == anomaly)
+        # if(dim(tempDataFrame)[1]==0)
+        #   next
+        # tempDataFrame <- reshape2::dcast(data = tempDataFrame, SAMPLEID ~ KEY, value.var = "FREQ", sum)
+        # mine.long <- tidyr::pivot_longer(data = tempDataFrame,
+        #                           cols = -c(1),
+        #                           names_to = "KEY",
+        #                           values_to = "FREQ")
+        # # head(mine.long)
+        # fillLable <- paste0("Number of ", anomaly, sep="" )
+        # mine.heatmap <- ggplot2::ggplot(data = mine.long,mapping = ggplot2::aes(x = KEY,y = SAMPLEID, fill = FREQ))
+        # mine.heatmap <- mine.heatmap +  ggplot2::geom_tile()
+        # mine.heatmap <- mine.heatmap + ggplot2::xlab(label = groupLabel)
+        # mine.heatmap <- mine.heatmap + ggplot2::ylab(label = "SAMPLE NAME")
+        # mine.heatmap <- mine.heatmap +  ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
+        #
+        # # filename = paste0( chartFolder,"/",paste0( pops, collapse ="_Vs_"),"_", groupLabel,"_",anomaly, ".png",sep="")
+        # # grDevices::png(file= filename, width=1200, height=1200)
+        # # mine.heatmap
+        # # grDevices::dev.off()
+        #
+        # filename = paste0( chartFolder,"/",paste0( pops, collapse ="_Vs_"),"_", groupLabel,"_",anomaly, ".png",sep="")
+        #
+        # try(
+        #   ggplot2::ggsave(
+        #     filename = filename,
+        #     plot = mine.heatmap,
+        #     device = NULL,
+        #     path = NULL,
+        #     scale = 1,
+        #     width = NA,
+        #     height = NA,
+        #     units = c("in", "cm", "mm"),
+        #     dpi = 1200,
+        #     limitsize = TRUE
+        #   )
+        # )
 
-        # col<- colorRampPalette(c("violet","white","blue"))(1024)
-
-        # skip heatmap if no enough data are available
-        tt <- tempDataFrame[,3:ncol(tempDataFrame)]
-        if (!is.null(tt))
-          if(!nrow(tt) < 2 & !ncol(tt) < 2)
+        tempDataFrame <- subset(inputBedDataFrame, inputBedDataFrame$ANOMALY == anomaly)
+        if(!is.null(tempDataFrame))
+          if(nrow(tempDataFrame)>2)
           {
-            filename = paste0( chartFolder,"/",paste0( pops, collapse ="_Vs_"),"_", groupLabel,"_",anomaly, ".png",sep="")
-            grDevices::png(file= filename, width=2480, height = 2480, pointsize = 15, res = 144)
-            stats::heatmap(as.matrix(tempDataFrame[,3:ncol(tempDataFrame)]),
-                           # col= colorRampPalette(RColorBrewer::brewer.pal(8, "Blues")),
-                           scale = "column",
-                           RowSideColors =as.vector(tempDataFrame$POPULATION),
-                           margins = c(25, 25),
-                           main = mainTitle
-            )
-            grDevices::dev.off()
+            tempDataFrame <- reshape2::dcast(data = tempDataFrame, SAMPLEID + POPULATION ~ KEY, value.var = "FREQ", sum)
+            row.names(tempDataFrame) <- tempDataFrame$SAMPLEID
+
+            mainTitle <- paste0( paste0( pops, collapse ="_Vs_")," ", groupLabel," ",anomaly, sep="")
+            if(nrow(tempDataFrame)>1000 || ncol(tempDataFrame)>1000)
+            {
+              #reduce
+              temp2 <- as.matrix(tempDataFrame[,3:dim(tempDataFrame)[2]])
+              temp <- apply(temp2,2, sum)
+              temp1 <- sort(temp, decreasing = T)
+              limit <- temp1[1000]
+              if(sum(temp1==limit)>1)
+                limit <- limit + 1
+              tempDataFrame <- data.frame(tempDataFrame[,1:2], temp2[,temp1 > limit])
+              rm(temp)
+              rm(temp1)
+              rm(temp2)
+              mainTitle <- paste0( mainTitle," (first 1000)",  sep="")
+            }
+
+            # col<- colorRampPalette(c("violet","white","blue"))(1024)
+
+            # skip heatmap if no enough data are available
+            tt <- tempDataFrame[,3:ncol(tempDataFrame)]
+            if (!is.null(tt))
+              if(!nrow(tt) < 2 & !ncol(tt) < 2)
+              {
+                filename = paste0( chartFolder,"/",paste0( pops, collapse ="_Vs_"),"_", groupLabel,"_",anomaly, ".png",sep="")
+                grDevices::png(file= filename, width=2480, height = 2480, pointsize = 15, res = 144)
+                stats::heatmap(as.matrix(tempDataFrame[,3:ncol(tempDataFrame)]),
+                               # col= colorRampPalette(RColorBrewer::brewer.pal(8, "Blues")),
+                               scale = "column",
+                               RowSideColors =as.vector(tempDataFrame$POPULATION),
+                               margins = c(25, 25),
+                               main = mainTitle
+                )
+                grDevices::dev.off()
+              }
           }
       }
-    }
     gc()
-}
+  }
