a62d8c113260c61d7dca73ce3adf8277c7d225d5
Luigi Corsaro <l.corsaro@a-company.it>
2022-05-27 08:38:57 +0200

    updated depency from stringi

--- tests/testthat/test-semseeker.R
+++ tests/testthat/test-semseeker.R
@@ -1,5 +1,6 @@
 test_that("semeeker", {
 
+  library(stringi)
   tempFolder <- paste("/tmp/semseeker/",stringi::stri_rand_strings(1, 7, pattern = "[A-Za-z0-9]"),sep="")
   envir <- init_env(tempFolder)
 
