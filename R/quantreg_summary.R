41c85c21f0b7181178bc0533d9319d93be4ff3a1
Luigi Corsaro <l.corsaro@a-company.it>
2022-06-13 19:32:08 +0200

    pvalue and confidence interval claculation

--- R/quantreg_summary.R
+++ R/quantreg_summary.R
@@ -8,59 +8,68 @@
 #' @param independent_variable name of indenpendent variable
 #' @param lqm_control controls of lqmm packages
 #'
-#' @return
+#' @return ci and pvalue with BCA method
 quantreg_summary <-function(boot_vector, estimate, working_data, sig.formula, tau, independent_variable, lqm_control){
 
-  alpha_seq <- seq(1e-16, 1-1e-16, 5e-6)
   #BCA method
   #Desired quantiles
+  alpha<-0.05
+  alpha_seq <- seq(1e-16, 1-1e-16, 5e-6)
 
-  u <- cbind(alpha_seq/2, 1-alpha_seq/2)
+  u <- c(alpha/2, 1-alpha/2)
+  u_seq <- cbind(alpha_seq/2, 1-alpha_seq/2)
 
   #Compute constants
   z0 <- stats::qnorm(mean(boot_vector <= estimate))
   zu <- stats::qnorm(u)
+  zu_seq <- stats::qnorm(u_seq)
 
-  # browser()
-  n<-nrow(working_data)
+  n<-length(working_data)
   I <- rep(NA, n)
-  jack <- rep(NA, n)
+  jack <- I
   i<-1
   for(i in 1:n){
     #Remove ith working_data point
-    xnew <- working_data[-i,]
+    working_data_new <- working_data[-i,]
 
     #Estimate beta dal modello
-    fit.lqm <- suppressMessages(lqmm::lqm( formula = sig.formula, data = xnew, tau = tau, control = lqm_control, fit = TRUE))
+    fit.lqm <- suppressMessages(lqmm::lqm( formula = sig.formula, data = working_data_new, tau = tau, control = lqm_control, fit = TRUE))
     fit.res <- suppressMessages(summary(fit.lqm)$tTable)
 
     jack[i] <-  fit.res[independent_variable,"Value"]
     I[i] <- (n-1)*(estimate -  jack[i] )
   }
+
   #Estimate acceleration
   acceleration <- (sum(I^3)/sum(I^2)^1.5)/6
+
+
+  #Adjusted quantiles
+  u_adjusted_seq <- stats::pnorm(z0 + (z0+zu_seq)/(1-acceleration*(z0+zu)))
+
+  #Accelerated Bootstrap CI
+  Bca1<-stats::quantile(boot_vector, u_adjusted_seq[,1])
+  Bca2<-stats::quantile(boot_vector, u_adjusted_seq[,2])
+  Bca<-cbind(Bca1,Bca2)
+  p.value <- alpha_seq[which.min(0 >= Bca[,1] & 0 <= Bca[,2])]
+
+  if(p.value==0)
+    p.value <- 1/( 1 + length(boot_vector))
+
+  #Estimate acceleration
+  acceleration <- (sum(I^3)/sum(I^2)^1.5)/6
+
+  acc.num <- sum((mean(jack)-jack)^3)
+  acc.denom <- sum((mean(jack)-jack)^2)
+  accelerate <- acc.num/(6*acc.denom^1.5)
 
   #Adjusted quantiles
   u_adjusted <- stats::pnorm(z0 + (z0+zu)/(1-acceleration*(z0+zu)))
 
   #Accelerated Bootstrap CI
-  Bca1<-stats::quantile(boot_vector, u_adjusted[,1])
-  Bca2<-stats::quantile(boot_vector, u_adjusted[,2])
-  selector <- which.min(0 >= Bca1 & 0 <= Bca2)
-  pval <- alpha_seq[selector]
-  if(pval==0)
-    pval <- 1/( 1 + length(boot_vector))
-
-
-  #Estimate a
-  a <- (sum(I^3)/sum(I^2)^1.5)/6
-  acc.num <- sum((mean(jack)-jack)^3)
-  acc.denom <- sum((mean(jack)-jack)^2)
-  accelerate <- acc.num/(6*acc.denom^1.5)
-  #Adjusted quantiles
-  u_adjusted <- stats::pnorm(z0 + (z0+zu)/(1-a*(z0+zu)))
-  #Accelerated Bootstrap CI
   Bca<-stats::quantile(boot_vector, u_adjusted)
 
-  return(c( Bca, pval))
+  return(c(Bca, p.value))
+
+
 }
