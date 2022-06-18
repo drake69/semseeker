c0d8e3fcedd20ffdbd5bf26eb3711222105082c7
Luigi Corsaro <l.corsaro@a-company.it>
2022-06-18 14:01:41 +0200

    variables renaming

--- tests/testthat/test-semseeker.R
+++ tests/testthat/test-semseeker.R
@@ -25,7 +25,8 @@ test_that("semeeker", {
   Sample_Group <- c(rep("Control",nsamples/3),rep("Reference",nsamples/3),rep("Case",nsamples/3))
   mySampleSheet <- data.frame(Sample_Group, Sample_ID)
 
-  semseeker( sample_sheet =  mySampleSheet,methylation_data =  methylation_data, result_folder = tempFolder, parallel_strategy = "multisession")
+  semseeker( sample_sheet =  mySampleSheet,methylation_data =  methylation_data, result_folder = tempFolder,
+             parallel_strategy = "multisession")
 
   tempresult_folder <- file.path(tempFolder,"Data","Control","MUTATIONS_BOTH")
   fileToRead <- file_path_build(tempresult_folder, c("MULTIPLE", "MUTATIONS" ,"BOTH" ), "bed")
