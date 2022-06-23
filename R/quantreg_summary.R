422ab9a2d03da93e48f073fa1e0e667e64af1bd8
Luigi Corsaro <l.corsaro@a-company.it>
2022-06-23 16:44:45 +0200

    added function to aggregate all pivots, removed jackknife pvalue and ci calculation, too expensive !!

--- R/quantreg_summary.R
+++ R/quantreg_summary.R
@@ -12,64 +12,64 @@
 #' @importFrom doRNG %dorng%
 quantreg_summary <-function(boot_vector, estimate, working_data, sig.formula, tau, independent_variable, lqm_control){
 
-  #BCA method
-  #Desired quantiles
-  alpha<-0.05
-  alpha_seq <- seq(1e-16, 1-1e-16, 5e-6)
+  # #BCA method
+  # #Desired quantiles
+  # alpha<-0.05
+  # alpha_seq <- seq(1e-16, 1-1e-16, 5e-6)
+  #
+  # alpha_interval <- c(alpha/2, 1-alpha/2)
+  # alpha_interval_seq <- cbind(alpha_seq/2, 1-alpha_seq/2)
+  #
+  # #Compute constants
+  # inverse_cumulative_normal_standard_alpha_of_mean_difference <- stats::qnorm(mean(boot_vector <= estimate), mean = 0, sd = 1)
+  # inverse_cumulative_normal_standard_alpha <- stats::qnorm(alpha_interval)
+  # inverse_cumulative_normal_standard_alpha_seq <- stats::qnorm(alpha_interval_seq)
+  #
+  # n<-length(working_data)
+  # I <- rep(NA, n)
+  # # jack <- I
+  # i<- 0
+  # to_export <- c("n", "working_data", "sig.formula", "tau", "lqm_control", "estimate", "independent_variable")
+  # # I <- foreach::foreach(i = 1:n, .combine = c, .export = to_export) %dorng%
+  # for(i in 1:n)
+  # {
+  #   #Remove ith working_data point
+  #   working_data_new <- working_data[-i,]
+  #
+  #   #Estimate beta dal modello
+  #   fit.lqm <- suppressMessages(lqmm::lqm( formula = sig.formula, data = working_data_new, tau = tau, control = lqm_control, fit = TRUE))
+  #   fit.res <- suppressMessages(summary(fit.lqm)$tTable)
+  #
+  #   # jack[i] <-  fit.res[independent_variable,"Value"]
+  #   I[i]<-(n-1)*(estimate -  fit.res[independent_variable,"Value"] )
+  # }
+  #
+  # #Estimate acceleration
+  # acceleration <- (sum(I^3)/sum(I^2)^1.5)/6
+  #
+  #
+  # #Adjusted quantiles
+  # u_adjusted_seq <- stats::pnorm(inverse_cumulative_normal_standard_alpha_of_mean_difference + (inverse_cumulative_normal_standard_alpha_of_mean_difference+inverse_cumulative_normal_standard_alpha_seq)/(1-acceleration*(inverse_cumulative_normal_standard_alpha_of_mean_difference+inverse_cumulative_normal_standard_alpha_seq)))
+  #
+  # #Accelerated Bootstrap CI
+  # Bca1<-stats::quantile(boot_vector, u_adjusted_seq[,1])
+  # Bca2<-stats::quantile(boot_vector, u_adjusted_seq[,2])
+  # Bca<-cbind(Bca1,Bca2)
+  # p.value <- alpha_seq[which.min(0 >= Bca[,1] & 0 <= Bca[,2])]
+  #
+  # if(p.value==0)
+  #   p.value <- 1/( 1 + length(boot_vector))
+  #
+  # #Adjusted quantiles
+  # u_adjusted <- stats::pnorm(inverse_cumulative_normal_standard_alpha_of_mean_difference + (inverse_cumulative_normal_standard_alpha_of_mean_difference+inverse_cumulative_normal_standard_alpha)/(1-acceleration*(inverse_cumulative_normal_standard_alpha_of_mean_difference+inverse_cumulative_normal_standard_alpha)))
+  #
+  # #Accelerated Bootstrap CI
+  # Bca<-stats::quantile(boot_vector, u_adjusted)
 
-  alpha_interval <- c(alpha/2, 1-alpha/2)
-  alpha_interval_seq <- cbind(alpha_seq/2, 1-alpha_seq/2)
-
-  #Compute constants
-  inverse_cumulative_normal_standard_alpha_of_mean_difference <- stats::qnorm(mean(boot_vector <= estimate), mean = 0, sd = 1)
-  inverse_cumulative_normal_standard_alpha <- stats::qnorm(alpha_interval)
-  inverse_cumulative_normal_standard_alpha_seq <- stats::qnorm(alpha_interval_seq)
-
-  n<-length(working_data)
-  I <- rep(NA, n)
-  # jack <- I
-  i<- 0
-  to_export <- c("n", "working_data", "sig.formula", "tau", "lqm_control", "estimate", "independent_variable")
-  # I <- foreach::foreach(i = 1:n, .combine = c, .export = to_export) %dorng%
-  for(i in 1:n)
-  {
-    #Remove ith working_data point
-    working_data_new <- working_data[-i,]
-
-    #Estimate beta dal modello
-    fit.lqm <- suppressMessages(lqmm::lqm( formula = sig.formula, data = working_data_new, tau = tau, control = lqm_control, fit = TRUE))
-    fit.res <- suppressMessages(summary(fit.lqm)$tTable)
-
-    # jack[i] <-  fit.res[independent_variable,"Value"]
-    I[i]<-(n-1)*(estimate -  fit.res[independent_variable,"Value"] )
-  }
-
-  #Estimate acceleration
-  acceleration <- (sum(I^3)/sum(I^2)^1.5)/6
-
-
-  #Adjusted quantiles
-  u_adjusted_seq <- stats::pnorm(inverse_cumulative_normal_standard_alpha_of_mean_difference + (inverse_cumulative_normal_standard_alpha_of_mean_difference+inverse_cumulative_normal_standard_alpha_seq)/(1-acceleration*(inverse_cumulative_normal_standard_alpha_of_mean_difference+inverse_cumulative_normal_standard_alpha_seq)))
-
-  #Accelerated Bootstrap CI
-  Bca1<-stats::quantile(boot_vector, u_adjusted_seq[,1])
-  Bca2<-stats::quantile(boot_vector, u_adjusted_seq[,2])
-  Bca<-cbind(Bca1,Bca2)
-  p.value <- alpha_seq[which.min(0 >= Bca[,1] & 0 <= Bca[,2])]
-
-  if(p.value==0)
-    p.value <- 1/( 1 + length(boot_vector))
-
-  #Adjusted quantiles
-  u_adjusted <- stats::pnorm(inverse_cumulative_normal_standard_alpha_of_mean_difference + (inverse_cumulative_normal_standard_alpha_of_mean_difference+inverse_cumulative_normal_standard_alpha)/(1-acceleration*(inverse_cumulative_normal_standard_alpha_of_mean_difference+inverse_cumulative_normal_standard_alpha)))
-
-  #Accelerated Bootstrap CI
-  Bca<-stats::quantile(boot_vector, u_adjusted)
-
-  # Bca <- coxed::bca(boot_vector)
-  # p.value <- 0
-  # if(Bca[1]<0 & Bca[2]>0)
-  #   p.value<- 1
+  Bca <- coxed::bca(boot_vector)
+  p.value <- 0
+  if(Bca[1]<0 & Bca[2]>0)
+    p.value<- 1
 
   return(c(Bca, p.value))
 }
