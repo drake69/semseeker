6716b45cd97d44e0bd7a6cadb27602185a5ad55a
Luigi Corsaro <l.corsaro@a-company.it>
2022-07-28 09:25:20 +0200

    debugged batch correlation analysis

--- R/batch_correlation_check.R
+++ R/batch_correlation_check.R
@@ -7,14 +7,14 @@ batch_correlation_check <- function(envir) {
   batch_analysis_folder <-dir_check_and_create(envir$result_folderData,"Batch_Analysis")
 
 
-  summary_cor <- foreach::foreach(i = 1:nrow(localKeys), .combine = rbind ) %dorng%
-    # for(i in 1:nrow(localKeys))
+  # summary_cor <- foreach::foreach(i = 1:nrow(localKeys), .combine = rbind ) %dorng%
+    for(i in 1:nrow(localKeys))
     {
       key <- localKeys[i,]
       populations <- unique(sample_sheet$Sample_Group)
       sub_export <- c("populations", "envir","key")
-      total_data_for <- foreach::foreach(g = 1:length(populations), .combine = rbind, .export = sub_export ) %dorng%
-        # for(g in 1:length(populations))
+      # total_data_for <- foreach::foreach(g = 1:length(populations), .combine = rbind, .export = sub_export ) %dorng%
+        for(g in 1:length(populations))
         {
           pop <- populations[g]
           tempresult_folderData <-dir_check_and_create(envir$result_folderData,c(as.character(pop) ,paste(as.character(key$ANOMALY),"_",as.character(key$FIGURE),sep="")))
@@ -22,11 +22,11 @@ batch_correlation_check <- function(envir) {
           temp <- fst::read_fst(file_to_read, as.data.table = T)
           if(key$ANOMALY=="MUTATIONS" | key$ANOMALY=="LESIONS")
             temp$VALUE <- 1
-          # if(exists("total_data_for"))
-          #   total_data_for <- rbind(total_data_for,temp)
-          # else
-          #   total_data_for <- temp
-          temp
+          if(exists("total_data_for"))
+            total_data_for <- rbind(total_data_for,temp)
+          else
+            total_data_for <- temp
+          # temp
         }
 
       total_data <- total_data_for
@@ -64,7 +64,8 @@ batch_correlation_check <- function(envir) {
       # calculate correlation
       pca_contrib <- pca_contrib[,-1]
 
-      cor.result <- Hmisc::rcorr(x = as.matrix(pca_contrib), type = c("pearson","spearman"))
+      cor.result <- Hmisc::rcorr(x = as.matrix(pca_contrib),
+                                 type = c("spearman"))
       result_cor <- data.frame("p.value"=cor.result$P[,"Batch_ID"], "rho"=cor.result$r[,"Batch_ID"])
       result_cor <- result_cor[order(result_cor$p.value),]
 
@@ -78,17 +79,18 @@ batch_correlation_check <- function(envir) {
       utils::write.csv2(result_cor,result_file,row.names = F)
 
       result_cor <- subset(result_cor, result_cor$p.value < 0.05)
+      result_cor <- subset(result_cor, result_cor$proportion < 0.7)
       if(!plyr::empty(result_cor))
       {
         result_cor$anomaly <- key$ANOMALY
         result_cor$figure <- key$FIGURE
       }
 
-      # if(exists("summary_cor"))
-      #   summary_cor <- rbind(result_cor,summary_cor)
-      # else
-      #   summary_cor <- result_cor
-      result_cor
+      if(exists("summary_cor"))
+        summary_cor <- rbind(result_cor,summary_cor)
+      else
+        summary_cor <- result_cor
+      # result_cor
     }
   result_file <- file_path_build(batch_analysis_folder, c("result","cor"), "csv")
   utils::write.csv2(summary_cor,result_file,row.names = F)
